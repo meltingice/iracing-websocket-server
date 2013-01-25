bb2b4350b0ed168b011ddd74ece8043a2352a3c1
meltingice <meltingice8917@gmail.com>
2013-01-25 13:30:29 -0500

    Move Javascript into its own file

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+(function () {
+  return;
+  var socket = io.connect('/');
+  socket.on('session', function (data) {
+    $("#Track").html(data.WeekendInfo.TrackDisplayName);
+
+    var telemetry = {};
+
+    socket.emit('addTelemetry', {name: 'Gear', onChange: true});
+    socket.emit('addTelemetry', {name: 'Throttle', onChange: true});
+    socket.emit('addTelemetry', {name: 'Brake', onChange: true});
+    socket.emit('addTelemetry', {name: 'Clutch', onChange: true});
+
+    socket.on('telemetry', function (data) {
+      if (data.name === "Gear") {
+        $("#Gear").html(data.value);
+      } else {
+        $("#" + data.name)
+          .find('> div')
+          .css('width', (data.value * 100) + "%");
+      }
+    });
+  });
+}());
